185355f1c504aebd59345a3797fb810eeb975f8a
Harry White <harry.white424@gmail.com>
2018-09-21 10:17:23 +1000

    Add check for deps on deplo

--- transformer/deploy.sh
+++ transformer/deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+if [[ ! -d "./ddi-transformer/node_modules" ]]; then
+  cd ./ddi-transformer
+  npm install
+  cd ..
+fi
+
 sam package --template-file template.yaml --s3-bucket aristotle-mdr-ddi-transformer --output-template-file packaged.yaml
 sam deploy --template-file packaged.yaml --stack-name ddi-transformer --capabilities CAPABILITY_IAM
